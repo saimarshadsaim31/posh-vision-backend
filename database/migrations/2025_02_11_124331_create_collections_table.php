22caee017592e0835e551f38a5c501d344db75b7
Ali <130615923+ghulamali04@users.noreply.github.com>
2025-02-11 18:06:03 +0500

    change user migration + create collection migration

---
+++ database/migrations/2025_02_11_124331_create_collections_table.php
@@ -0,0 +1,38 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('collections', function (Blueprint $table) {
+            $table->id();
+            $table->timestamps();
+            $table->softDeletes();
+            $table->unsignedBigInteger('user_id')->index();
+            $table->foreign('user_id', 'fk_collection_user_id')
+            ->references('id')
+            ->on('users')
+            ->onDelete('cascade');
+            $table->text('title');
+            $table->text('shopify_collection_id')->nullable();
+            $table->longText('image')->nullable();
+            $table->longText('description')->nullable();
+            $table->string('shopify_publication_status');
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('collections');
+    }
+};
